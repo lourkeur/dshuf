d23fdae793657ecf1f622fea167029ab3d9036f4
Louis Bettens <louis@bettens.info>
2023-02-27 14:42:26 +0100

    use blake3

--- src/pick.rs
+++ src/pick.rs
@@ -1,14 +1,16 @@
 use num_bigint::BigUint;
 use num_traits::cast::ToPrimitive;
-use sha2::{Digest, Sha256};
 
-pub fn pick<T>(randomness: &[u8], mut input: Vec<T>, output_len: usize) -> Vec<T> {
+const SAMPLE_LEN: usize = 24;
+
+pub fn pick<T>(randomness: &[u8; 32], mut input: Vec<T>, output_len: usize) -> Vec<T> {
     let input_len = input.len();
-    for i in 0..output_len {
-        let mut h = Sha256::new();
-        h.update(randomness);
-        h.update(BigUint::from(i).to_bytes_be());
-        let r = BigUint::from_bytes_be(&h.finalize()[..]);
+    let mut samples = vec![0u8;SAMPLE_LEN*output_len];
+    let h = blake3::Hasher::new_keyed(randomness);
+    // TODO: seed prng with shuffle inputs
+    h.finalize_xof().fill(samples.as_mut_slice());
+    for (i, sample) in samples.chunks(SAMPLE_LEN).enumerate() {
+        let r = BigUint::from_bytes_be(sample);
         input.swap(i, i + (r % (input_len - i)).to_usize().unwrap());
     }
     input.resize_with(output_len, || panic!("unreachable"));
