2a09ada85bf640d9e7deb04800ecded8f4920d43
Louis Bettens <louis@bettens.info>
2023-03-11 18:32:26 +0100

    rust: handle zero-length gracefully

--- rust/src/core.rs
+++ rust/src/core.rs
@@ -1,6 +1,8 @@
 use num_bigint::BigUint;
 use num_traits::cast::ToPrimitive;
 
+use core::cmp::min;
+
 const SAMPLE_LEN: usize = 24;
 
 pub fn shuffle<'a>(
@@ -9,19 +11,19 @@ pub fn shuffle<'a>(
     limit: usize,
 ) -> Vec<&'a [u8]> {
     let input_len = input.len();
-    assert!(limit <= input_len);
+    let output_len = min(limit, input_len);
     let mut h = blake3::Hasher::new_keyed(randomness);
     for e in &input {
         h.update(&(e.len() as u64).to_be_bytes());
         h.update(e);
     }
     let mut prng = h.finalize_xof();
-    for i in 0..limit {
+    for i in 0..output_len {
         let mut sample = [0u8; SAMPLE_LEN];
         prng.fill(&mut sample);
         let r = BigUint::from_bytes_be(&sample);
         input.swap(i, i + (r % (input_len - i)).to_usize().unwrap());
     }
-    input.resize_with(limit, || unreachable!());
+    input.truncate(output_len);
     input
 }
